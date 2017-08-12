2f85949ff2e9d12007ea066c2a3d65507136bae3
caristopmer <chris.mcmenomy@gmail.com>
2017-08-12 16:26:10 -0500

    initial routes and nav set up.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  get '/login' => 'sessions#new', as: "new_session"
+  delete '/sessions' => 'sessions#destroy'
+  get '/users/:id' => 'users#show', as: "user"
 end
