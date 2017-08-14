0b38ac233a8d17ee1776d1e4497aab9035c8e6cc
caristopmer <chris.mcmenomy@gmail.com>
2017-08-14 09:58:07 -0500

    completed writing associations for heats, entries, power levels.

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,5 +1,5 @@
 class Entry < ApplicationRecord
+  belongs_to :user
 
   validates :user_id, :weight, :weight_unit, presence: true
-
 end
--- app/models/power_level.rb
+++ app/models/power_level.rb
@@ -1,5 +1,5 @@
 class PowerLevel < ApplicationRecord
+  belongs_to :user
 
   validates :user_id, :level, presence: true
-
 end
