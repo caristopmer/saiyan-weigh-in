08218ea0b5008e092d63574ec14065cdd87b3ef4
caristopmer <chris.mcmenomy@gmail.com>
2017-08-17 17:32:03 -0500

    User teams showing up on profile page, added view folder and files for teams.

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,3 +1,23 @@
 class TeamsController < ApplicationController
 
+  def index
+    
+  end
+
+  def show
+    @team = Team.find(params[:id])
+  end
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+  def destroy
+    
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
 
-User.create(username: "izzlebeast", email: "chris@chris.com", password: "password", password_confirmation: "password", height: 74, height_unit: "inches")
-User.create(username: "AmandaH", email: "amanda@amanda.com", password: "password", password_confirmation: "password", height: 67, height_unit: "inches")
+# User.create(username: "izzlebeast", email: "chris@chris.com", password: "password", password_confirmation: "password", height: 74, height_unit: "inches")
+# User.create(username: "AmandaH", email: "amanda@amanda.com", password: "password", password_confirmation: "password", height: 67, height_unit: "inches")
 
 # Entry.create(user_id: 1, weight: 220, weight_unit: "lbs.", entry_date: Date.today - 5)
 # Entry.create(user_id: 1, weight: 210, weight_unit: "lbs.", entry_date: Date.today - 3)
 # Entry.create(user_id: 1, weight: 200, weight_unit: "lbs.", entry_date: Date.today)
+
+team = Team.create(name: "The Maddi Monsters", captain: User.find_by(username: "izzlebeast"))
+
+team.members << User.find_by(username: "izzlebeast")
+team.members << User.find_by(username: "AmandaH")
+team.save
