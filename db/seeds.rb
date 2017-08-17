4f2b6c1ddf4ddbb95023f470a69362b7cd849e08
caristopmer <chris.mcmenomy@gmail.com>
2017-08-17 12:24:18 -0500

    update seed file to only seed chris and amanda users.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,6 @@
 User.create(username: "izzlebeast", email: "chris@chris.com", password: "password", password_confirmation: "password", height: 74, height_unit: "inches")
 User.create(username: "AmandaH", email: "amanda@amanda.com", password: "password", password_confirmation: "password", height: 67, height_unit: "inches")
 
-Entry.create(user_id: 1, weight: 220, weight_unit: "lbs.", entry_date: Date.today - 5)
-Entry.create(user_id: 1, weight: 210, weight_unit: "lbs.", entry_date: Date.today - 3)
-Entry.create(user_id: 1, weight: 200, weight_unit: "lbs.", entry_date: Date.today)
+# Entry.create(user_id: 1, weight: 220, weight_unit: "lbs.", entry_date: Date.today - 5)
+# Entry.create(user_id: 1, weight: 210, weight_unit: "lbs.", entry_date: Date.today - 3)
+# Entry.create(user_id: 1, weight: 200, weight_unit: "lbs.", entry_date: Date.today)
