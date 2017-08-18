5eab2628ceea48b74c2864f5177b62fa828ab7ae
caristopmer <chris.mcmenomy@gmail.com>
2017-08-18 09:28:19 -0500

    Team creation and index working. User's teams are listed in profile.

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -9,15 +9,28 @@ class TeamsController < ApplicationController
   end
 
   def new
-    
   end
 
   def create
-    
+    @team = Team.new(team_params)
+    if @team.save
+      @team.members << @team.captain
+      @team.save
+      redirect_to team_path(@team)
+    else
+      @errors = @team.errors.full_messages
+      render "teams/new"
+    end
   end
 
   def destroy
     
+  end
+
+  private
+
+  def team_params
+    params.require(:team).permit(:name, :captain_id)
   end
 
 end
