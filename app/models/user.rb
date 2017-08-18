ac960fab4b6befbb6ffde368f149e27d229c10e4
caristopmer <chris.mcmenomy@gmail.com>
2017-08-18 18:30:18 -0500

    user weight stats on profile page working, average weight calculations need reworking. They don't currently work properly if adding older entries.

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,14 +10,32 @@ class User < ApplicationRecord
   validates :username, :email, :height, :height_unit, presence: true
   validates :username, :email, uniqueness: true
 
-  def weekly_change(weeks = nil)
+  def change_over_period(weeks = nil)
     if weeks
       entries = self.entries.where(["entry_date > ?", Date.today - (weeks * 7)]).order(entry_date: :desc)
     else
       entries = self.entries.order(entry_date: :desc)
-      weeks = (entries.first.entry_date - entries.last.entry_date) / 7
     end
-    delta = (entries.first.average_weight - entries.last.weight) / weeks
+    (entries.first.average_weight - entries.last.weight).round(2)
+  end
+
+  def weekly_change(weeks = nil)
+    if weeks
+      delta = change_over_period(weeks) / weeks
+    else
+      if total_weeks < 4
+        delta = change_over_period(weeks) / 4
+      else
+        delta = change_over_period(weeks) / total_weeks
+      end
+    end
     delta.round(2)
+  end
+
+  private
+
+  def total_weeks
+    entries = self.entries.order(entry_date: :desc)
+    (entries.first.entry_date - entries.last.entry_date) / 7
   end
 end
