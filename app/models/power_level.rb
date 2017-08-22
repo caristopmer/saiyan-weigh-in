739401fb830f778aefb74884a79bb7fd74eada5d
caristopmer <chris.mcmenomy@gmail.com>
2017-08-21 21:21:50 -0500

    pseudocoded process of power level calculation in power level.rb

--- app/models/power_level.rb
+++ app/models/power_level.rb
@@ -2,4 +2,11 @@ class PowerLevel < ApplicationRecord
   belongs_to :user
 
   validates :user_id, :level, :entry_date, presence: true
+
+  # user decides to calculate their power level on a specified date
+  # post request is made to power levels
+  # power level is created for currently logged in user
+  # level is determined from that user's data at time of entry date
+  # current bmi points act as multiplier and set a baseline, each pushup adds points to power level as multiplied by bmi multiplier
+  
 end
