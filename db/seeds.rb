e6aa7c78176f6429e90380b1e3184ffece92ffad
caristopmer <chris.mcmenomy@gmail.com>
2017-09-03 13:39:29 -0500

    working on updating seeds with guest user and entries.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,11 @@
 
-User.create(username: "izzlebeast", email: "chris@chris.com", password: "password", password_confirmation: "password", height: 74, height_unit: "inches")
-User.create(username: "AmandaH", email: "amanda@amanda.com", password: "password", password_confirmation: "password", height: 67, height_unit: "inches")
+User.create(username: "Guest", email: "guest@guest.com", password: "password", password_confirmation: "password", height: 72, height_unit: "inches")
 
-# Entry.create(user_id: 1, weight: 220, weight_unit: "lbs.", entry_date: Date.today - 5)
-# Entry.create(user_id: 1, weight: 210, weight_unit: "lbs.", entry_date: Date.today - 3)
-# Entry.create(user_id: 1, weight: 200, weight_unit: "lbs.", entry_date: Date.today)
+Entry.create(user: User.find_by(username: "Guest"), weight: 300.0, weight_unit: "lbs.", entry_date: "2017-03-10")
+
+
+
+
 
 team = Team.create(name: "The Maddi Monsters", captain: User.find_by(username: "izzlebeast"))
 
